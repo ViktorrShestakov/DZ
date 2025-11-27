7055a79bec38cf2626398ba728dd78de4f26e134
ViktorrShestakov <damnfox666@yandex.com>
2025-11-27 14:11:07 +0300

    Исправления 2

--- src/main/kotlin/Task2.kt
+++ src/main/kotlin/Task2.kt
@@ -1,11 +1,11 @@
 fun main() {
-    val a1= readln().toInt()
-    val a2= readln().toInt()
-    if (a1<a2){
+    val a1 = readln().toInt()
+    val a2 = readln().toInt()
+    if (a1 < a2) {
         for (i in a1..a2) {
             println(i)
         }
-    } else{
+    } else {
         for (i in a2..a1) {
             println(i)
         }
--- src/main/kotlin/Task4.kt
+++ src/main/kotlin/Task4.kt
@@ -1,13 +1,13 @@
-fun main(){
-    var n=0
-    var stroka= readln()
-    while (stroka!="exit"){
-        when(stroka){
-            "increase"->n++
-            "decrease"->n--
-            else-> println("Ошибка! Команда не найдена")
+fun main() {
+    var n = 0
+    var stroka = readln()
+    while (stroka != "exit") {
+        when (stroka) {
+            "increase" -> n++
+            "decrease" -> n--
+            else -> println("Ошибка! Команда не найдена")
         }
-        stroka= readln()
+        stroka = readln()
     }
     println(n)
 }
